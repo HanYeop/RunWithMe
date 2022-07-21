24a8ce0fabaf55bfe63814302e1f0108a576d510
dkeka1187 <dkeka1187@naver.com>
2022-07-21 21:32:30 +0900

    [AOS] - ADD : 마이페이지 화면 완성

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/MyPageFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/MyPageFragment.kt
@@ -1,11 +1,50 @@
 package com.ssafy.runwithme.view.my_page
 
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentMyPageBinding
+import com.ssafy.runwithme.view.my_page.tab.achievement.AchievementFragment
+import com.ssafy.runwithme.view.my_page.tab.my_board.MyBoardFragment
+import com.ssafy.runwithme.view.my_page.tab.total_record.MyTotalRunRecordFragment
 
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
-    override fun init() {
+    private lateinit var myBoardFragment: MyBoardFragment
+    private lateinit var myTotalRunRecordFragment: MyTotalRunRecordFragment
+    private lateinit var achievementFragment: AchievementFragment
 
+    override fun init() {
+        initTabLayout()
     }
+
+    // 각 탭에 들어갈 프레그먼트 객체화
+    private fun initTabLayout(){
+        myTotalRunRecordFragment = MyTotalRunRecordFragment()
+        myBoardFragment = MyBoardFragment()
+        achievementFragment = AchievementFragment()
+
+        childFragmentManager.beginTransaction().replace(R.id.frame_layout_my_page, myTotalRunRecordFragment).commit()
+
+        binding.tabLayoutMyPage.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab!!.position) {
+                    0 -> replaceView(myTotalRunRecordFragment)
+                    1 -> replaceView(myBoardFragment)
+                    2 -> replaceView(achievementFragment)
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+
+    // 화면 변경
+    private fun replaceView(tab : Fragment){
+        var selectedFragment = tab
+        selectedFragment.let {
+            childFragmentManager.beginTransaction().replace(R.id.frame_layout_my_page, it).commit()
+        }
+    }
+
 }
