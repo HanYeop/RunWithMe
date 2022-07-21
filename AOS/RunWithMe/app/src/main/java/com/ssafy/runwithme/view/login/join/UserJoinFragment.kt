1bfa0a4de95ec83aff0191fde5c515f359db46ec
dkeka1187 <dkeka1187@naver.com>
2022-07-21 18:25:33 +0900

    [AOS] - UPDATE : join 화면 spinner 추가

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/join/UserJoinFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/join/UserJoinFragment.kt
@@ -1,11 +1,36 @@
 package com.ssafy.runwithme.view.login.join
 
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentUserJoinBinding
 
 class UserJoinFragment : BaseFragment<FragmentUserJoinBinding>(R.layout.fragment_user_join) {
     override fun init() {
+        val heightList = Array(130) { i -> i + 120 }
+        val weightList = Array(230) { i -> i + 20 }
 
+        binding.spinnerHeight.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, heightList)
+        binding.spinnerHeight.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                showToast((position + 120).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { }
+        }
+
+        binding.spinnerWeight.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, weightList)
+        binding.spinnerWeight.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                showToast((position + 20).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) { }
+        }
     }
+
 }
