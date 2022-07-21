50896bb05e47185f3e873065ffa24ac51fa58344
super7615 <super7615@naver.com>
2022-07-21 10:08:30 +0900

    [AOS] - UPDATE : toolbar 마진 변경

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/crew_detail/CrewDetailFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/crew_detail/CrewDetailFragment.kt
@@ -1,11 +1,20 @@
 package com.ssafy.runwithme.view.crew_detail
 
+import androidx.navigation.fragment.findNavController
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentCrewDetailBinding
 
 class CrewDetailFragment : BaseFragment<FragmentCrewDetailBinding>(R.layout.fragment_crew_detail) {
     override fun init() {
+        initClickListener()
+    }
 
+    private fun initClickListener(){
+        binding.apply {
+            toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
