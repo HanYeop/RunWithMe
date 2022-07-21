454cc56217a3a52c01fe20d1f92c03858e90a642
super7615 <super7615@naver.com>
2022-07-21 09:51:06 +0900

    [AOS]  -  ADD : runningFragment

---
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/running/RunningFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.runwithme.view.running
+
+import com.ssafy.runwithme.R
+import com.ssafy.runwithme.base.BaseFragment
+import com.ssafy.runwithme.databinding.FragmentRunningBinding
+
+class RunningFragment : BaseFragment<FragmentRunningBinding>(R.layout.fragment_running) {
+    override fun init() {
+
+    }
+}
