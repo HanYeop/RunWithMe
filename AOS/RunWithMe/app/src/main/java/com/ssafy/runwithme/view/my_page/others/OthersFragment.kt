8324c7ea040db38feca6eca6c90bfee07a1bdabe
dkeka1187 <dkeka1187@naver.com>
2022-07-21 22:16:44 +0900

    [AOS] - ADD : 환경설정 UI 완료

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/others/OthersFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/others/OthersFragment.kt
@@ -1,11 +1,20 @@
 package com.ssafy.runwithme.view.my_page.others
 
+import androidx.navigation.fragment.findNavController
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentOthersBinding
 
 class OthersFragment : BaseFragment<FragmentOthersBinding>(R.layout.fragment_others) {
     override fun init() {
+        initClickListener()
+    }
 
+    private fun initClickListener(){ // 뒤로가기 버튼
+        binding.apply {
+            toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
