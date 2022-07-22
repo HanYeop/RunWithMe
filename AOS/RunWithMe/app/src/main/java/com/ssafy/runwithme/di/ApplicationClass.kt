48db72d8e96d3ff0085a33bc96c2a389da159378
super7615 <super7615@naver.com>
2022-07-22 18:29:00 +0900

    [AOS] - ADD : 구글 로그인, 카카오 로그인 TEST

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/di/ApplicationClass.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/di/ApplicationClass.kt
@@ -1,8 +1,19 @@
 package com.ssafy.runwithme.di
 
 import android.app.Application
+import android.content.Context
+import com.kakao.sdk.common.KakaoSdk
+import com.ssafy.runwithme.R
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class ApplicationClass: Application() {
+    companion object {
+        var appContext : Context? = null
+    }
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+        KakaoSdk.init(this,getString(R.string.kakao_app_key))
+    }
 }
--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/login/UserLoginFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/login/UserLoginFragment.kt
@@ -9,8 +9,14 @@ import androidx.fragment.app.viewModels
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.Scopes
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
 import com.google.android.gms.tasks.Task
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentUserLoginBinding
@@ -33,11 +39,15 @@ class UserLoginFragment : BaseFragment<FragmentUserLoginBinding>(R.layout.fragme
             imgBtnLoginGoogle.setOnClickListener {
                 googleSignIn()
             }
+            imgBtnLoginKakao.setOnClickListener {
+                kakaoSignIn()
+            }
         }
     }
 
     private fun googleSignIn() {
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope(Scopes.EMAIL))
             .requestServerAuthCode(resources.getString(R.string.google_client_key))
             .requestEmail()
             .build()
@@ -57,12 +67,78 @@ class UserLoginFragment : BaseFragment<FragmentUserLoginBinding>(R.layout.fragme
                 val accessToken = account.serverAuthCode!!
                 showToast(accessToken)
                 Log.d("test5", ":$accessToken ")
-//                viewModel.socialLogin(GOOGLE, accessToken)
             } catch (e: ApiException) {
                 Log.w("test5", "signInResult:failed code=" + e.statusCode)
             }
         } else {
             Log.d("test5", "$it: ")
+        }
+    }
+
+    val TAG = "test5"
+    private fun kakaoSignIn(){
+        Log.e(TAG, "카카오1")
+        // 카카오계정으로 로그인 공통 callback 구성
+        // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            Log.e(TAG, "카카오8")
+            if (error != null) {
+                Log.e(TAG, "카카오2")
+                Log.e(TAG, "카카오계정으로 로그인 실패", error)
+            } else if (token != null) {
+                Log.e(TAG, "카카오3")
+                Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+                // 사용자 정보 요청 (기본)
+                UserApiClient.instance.me { user, error ->
+                    if (error != null) {
+                        Log.e(TAG, "카카오4")
+                        Log.e(TAG, "사용자 정보 요청 실패", error)
+                    }
+                    else if (user != null) {
+                        Log.e(TAG, "카카오5")
+                        Log.i(TAG, "사용자 정보 요청 성공" +
+                                "\n회원번호: ${user.id}" +
+                                "\n이메일: ${user.kakaoAccount?.email}" +
+                                "\n닉네임: ${user.kakaoAccount?.profile?.nickname}")
+                    }else{
+                        Log.e(TAG, "카카오6")
+                    }
+                }
+            }else{
+                Log.e(TAG, "카카오7")
+            }
+        }
+
+        // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+                } else if (token != null) {
+                    UserApiClient.instance.me { user, error ->
+                        if (error != null) {
+                            Log.e(TAG, "사용자 정보 요청 실패", error)
+                        }
+                        else if (user != null) {
+                            Log.i(TAG, "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n이메일: ${user.kakaoAccount?.email}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}")
+                        }
+                    }
+                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
         }
     }
 }
