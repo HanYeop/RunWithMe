e0ea900422fc94fa8f30b9390351702fcdf2d054
super7615 <super7615@naver.com>
2022-07-22 13:24:58 +0900

    [AOS] - ADD : Remote DI

---
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/di/ApplicationClass.kt
@@ -0,0 +1,8 @@
+package com.ssafy.runwithme.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationClass: Application() {
+}
---
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/di/RemoteDataModule.kt
@@ -0,0 +1,61 @@
+package com.ssafy.runwithme.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.ssafy.runwithme.api.Oauth2Api
+import com.ssafy.runwithme.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteDataModule {
+
+    // Interceptor DI
+    @Provides
+    @Singleton
+    fun provideInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    // OkHttpClient DI
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+    }
+
+    // Gson DI
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().setLenient().create()
+    }
+
+    // Retrofit DI
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(gson: Gson, client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(client)
+            .build()
+    }
+
+    // Oauth2Api DI
+    @Provides
+    @Singleton
+    fun provideOauth2Api(retrofit: Retrofit): Oauth2Api {
+        return retrofit.create(Oauth2Api::class.java)
+    }
+}
