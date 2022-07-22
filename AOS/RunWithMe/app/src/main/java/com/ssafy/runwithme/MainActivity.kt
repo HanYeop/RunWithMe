eff2153a4f6aebdab0880044bd7a89d997611ea1
super7615 <super7615@naver.com>
2022-07-23 00:21:16 +0900

    [AOS] - ADD : 메인 페이지에서 위치 권한 요구

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/MainActivity.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/MainActivity.kt
@@ -1,10 +1,65 @@
 package com.ssafy.runwithme
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.ssafy.runwithme.base.BaseActivity
 import com.ssafy.runwithme.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    override fun init() {
 
+    override fun init() {
+        requestPermission{
+            if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                == PackageManager.PERMISSION_DENIED) {
+                permissionDialog()
+            }
+        }
+    }
+
+    private fun requestPermission(logic : () -> Unit){
+        TedPermission.create()
+            .setPermissionListener(object : PermissionListener {
+                override fun onPermissionGranted() {
+                    logic()
+                }
+                override fun onPermissionDenied(deniedPermissions: List<String>) {
+                    showToast("권한을 허가해주세요.")
+                }
+            })
+            .setDeniedMessage("백그라운드 위치 권한을 위해 항상 허용으로 설정해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
+            .setPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+            .check()
+    }
+
+    private fun permissionDialog(){
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("백그라운드 위치 권한을 위해 항상 허용으로 설정해주세요.")
+
+        var listener = DialogInterface.OnClickListener { _, p1 ->
+            when (p1) {
+                DialogInterface.BUTTON_POSITIVE ->
+                    backgroundPermission()
+            }
+        }
+        builder.setPositiveButton("네", listener)
+        builder.setNegativeButton("아니오", null)
+
+        builder.show()
+    }
+
+    private fun backgroundPermission(){
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            ), 2)
     }
 }
