88e9bf86400986ef1c2c13e1c74dbe76af9509c2
한제규 <anfidthtn@naver.com>
2022-07-23 20:05:34 +0900

    [BE] - ADD : 크루 목록 조회 기능 추가

--- BE/gumid101/src/main/java/com/ssafy/gumid101/crew/CrewActivityRestController.java
+++ BE/gumid101/src/main/java/com/ssafy/gumid101/crew/CrewActivityRestController.java
@@ -1,7 +1,10 @@
 package com.ssafy.gumid101.crew;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,20 +19,45 @@ import com.ssafy.gumid101.dto.CrewBoardDto;
 import com.ssafy.gumid101.dto.RankingParamsDto;
 import com.ssafy.gumid101.dto.RecordParamsDto;
 import com.ssafy.gumid101.dto.RunRecordDto;
+import com.ssafy.gumid101.res.ResponseFrame;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/crew-activity")
+@RequiredArgsConstructor
+@Api(tags = "크루 글, 기록, 달리기 등 크루 활동 API")
 public class CrewActivityRestController {
 	
-	private CrewActivityService crewActivityService;
+	private final CrewActivityService crewActivityService;
 	
 	public RequestEntity<?> getCrewRecord(){
 		return null;
 	}
 	
-	@GetMapping("/{crewSeq}/records")
-	public RequestEntity<?> getCrewRecordList(@ModelAttribute RecordParamsDto recordParamsDto){
-		return null;
+	@ApiOperation(value = "검색 조건에 따라 기록의 목록을 조회한다. (URL의 crewSeq만 적용)")
+	@GetMapping("/{crewSeq_p}/records")
+	public ResponseEntity<?> getCrewRecordList(@PathVariable(name = "crewSeq_p") String crewSeq, @ModelAttribute RecordParamsDto recordParamsDto){
+//		log.debug("크루 생성 시도 : 이름 {}, 설명 {}, ", crewDto.getCrewName());
+		recordParamsDto.setCrewSeq(Long.parseLong(crewSeq));
+		ResponseFrame<List<RunRecordDto>> responseMap = new ResponseFrame<>();
+		HttpStatus httpStatus = HttpStatus.OK;
+		
+		List<RunRecordDto> crewRecordList = crewActivityService.getCrewRecordList(recordParamsDto);
+
+		if (crewRecordList == null) {
+			httpStatus = HttpStatus.CONFLICT;
+			responseMap.setCount(0);
+			responseMap.setSuccess(false);
+		} else {
+			responseMap.setCount(crewRecordList.size());
+			responseMap.setSuccess(true);
+		}
+		responseMap.setData(crewRecordList);
+
+		return new ResponseEntity<>(responseMap, httpStatus);
 	}
 	
 	@GetMapping("/{crewSeq}/ranking")
