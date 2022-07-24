a3ed0d04bcdfd357757a8e1ddb214287e6a4d726
dkeka1187 <dkeka1187@naver.com>
2022-07-24 19:48:22 +0900

    [AOS] - UPDATE : 권한 설정 코드 단순화

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/MainActivity.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/MainActivity.kt
@@ -29,13 +29,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         requestPermission{
             if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
                 == PackageManager.PERMISSION_DENIED) {
-                permissionDialog("백그라운드 위치 권한을 위해 항상 허용으로 설정해주세요.")
-            }
-        }
-        galleryRequestPermission{
-            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_DENIED) {
-                permissionDialog("사진 접근 권한을 위해 항상 허용으로 설정해주세요.")
+                permissionDialog()
             }
         }
         initNavigation()
@@ -67,20 +61,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         }
     }
 
-    private fun galleryRequestPermission(logic : () -> Unit){
-        TedPermission.create()
-            .setPermissionListener(object : PermissionListener {
-                override fun onPermissionGranted() {
-                    logic()
-                }
-                override fun onPermissionDenied(deniedPermissions: List<String>) {
-                    showToast("권한을 허가해주세요.")
-                }
-            })
-            .setDeniedMessage("사진 접근 권한을 위해 항상 허용으로 설정해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
-            .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
-            .check()
-    }
 
     private fun requestPermission(logic : () -> Unit){
         TedPermission.create()
@@ -92,37 +72,28 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                     showToast("권한을 허가해주세요.")
                 }
             })
-            .setDeniedMessage("백그라운드 위치 권한을 위해 항상 허용으로 설정해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
+            .setDeniedMessage("앱 사용을 위해 권한을 허용으로 설정해주세요. [설정] > [앱 및 알림] > [고급] > [앱 권한]")
             .setPermissions(
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_EXTERNAL_STORAGE
             )
             .check()
     }
 
-    private fun permissionDialog(msg : String){
+    private fun permissionDialog(){
         var builder = AlertDialog.Builder(this)
-        builder.setTitle(msg)
+        builder.setTitle("백그라운드 위치 권한을 위해 항상 허용으로 설정해주세요.")
 
         var listener = DialogInterface.OnClickListener { _, p1 ->
             when (p1) {
-                DialogInterface.BUTTON_POSITIVE ->
-                    if(msg.startsWith('백')) backgroundPermission()
-                    else galleryPermission()
+                DialogInterface.BUTTON_POSITIVE -> backgroundPermission()
             }
         }
         builder.setPositiveButton("네", listener)
         builder.setNegativeButton("아니오", null)
 
         builder.show()
-    }
-
-    private fun galleryPermission(){
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-            ), 2)
     }
 
     private fun backgroundPermission(){
