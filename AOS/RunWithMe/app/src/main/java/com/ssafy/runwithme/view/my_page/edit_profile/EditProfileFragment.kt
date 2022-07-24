d475b336213d981becca072fb8ef29a11ec5eafb
dkeka1187 <dkeka1187@naver.com>
2022-07-24 19:13:47 +0900

    [AOS] - ADD : 프로필 변경 페이미 기기 사진 가져오기 추가

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/edit_profile/EditProfileFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/edit_profile/EditProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.ssafy.runwithme.view.my_page.edit_profile
 
+import android.app.Activity
+import android.content.Intent
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -9,6 +11,9 @@ import com.ssafy.runwithme.base.BaseFragment
 import com.ssafy.runwithme.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fragment_edit_profile) {
+
+    private val IMAGE_PICK_CODE = 1000;
+
     override fun init() {
         initClickListener()
         initSpinner() // 키와 몸무게 스피너 값 넣기
@@ -16,14 +21,30 @@ class EditProfileFragment : BaseFragment<FragmentEditProfileBinding>(R.layout.fr
 
     private fun initClickListener() {
         binding.apply {
-            toolbar.setNavigationOnClickListener {
-                findNavController().popBackStack()
-            }
             btnModify.setOnClickListener {
                 findNavController().popBackStack()
             }
+            // 프로필 이미지 변경 아이콘 클릭
+            imageEditPhoto.setOnClickListener {
+                pickPhotoGallery()
+            }
         }
     }
+
+    private fun pickPhotoGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            binding.imageUserProfile.setImageURI(data?.data)
+        }
+    }
+
 
     private fun initSpinner(){
         val heightList = Array(131) { i -> i + 120 }
