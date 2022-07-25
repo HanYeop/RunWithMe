6f49916f568bdeac7fa8444d051fa873e2727ad5
한제규 <anfidthtn@naver.com>
2022-07-25 09:41:37 +0900

    [BE] - UPDATE : LOCALDATETIME, LOCALTIME 입력형식 지정

--- BE/gumid101/src/main/java/com/ssafy/gumid101/dto/CrewDto.java
+++ BE/gumid101/src/main/java/com/ssafy/gumid101/dto/CrewDto.java
@@ -6,8 +6,10 @@ import java.time.LocalTime;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ssafy.gumid101.entity.CrewEntity;
 
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,30 +28,48 @@ public class CrewDto implements Serializable {
 
 	private Long crewSeq;
 
+	@ApiParam(value = "크루 이름", required = true)
 	private String crewName;
 	
+	@ApiParam(value = "크루 설명")
 	private String crewDescription;
 	
+	@ApiParam(value = "한 주당 목표 활동일수", required = true)
 	private Integer crewGoalDays;
 	
+	@ApiParam(value = "크루 목표 형태", required = true)
 	private String crewGoalType;
 	
+	@ApiParam(value = "크루 목표 수치", required = true)
 	private Integer crewGoalAmount;
 	
+	@ApiParam(value = "크루 활동 시작 날짜. (yyyy-MM-dd 00:00:00)", required = true)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime crewDateStart;
 	
+	@ApiParam(value = "크루 활동 종료 날짜. (yyyy-MM-dd 23:59:59)", required = true)
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime crewDateEnd;
 	
+	@ApiParam(value = "크루 활동 시작 시간. (HH:mm:ss)", required = true)
 	@DateTimeFormat(pattern = "HH:mm:ss")
+	@JsonFormat(pattern = "HH:mm:ss")
 	private LocalTime crewTimeStart;
 
+	@ApiParam(value = "크루 활동 종료 시간. (HH:mm:ss)", required = true)
 	@DateTimeFormat(pattern = "HH:mm:ss")
+	@JsonFormat(pattern = "HH:mm:ss")
 	private LocalTime crewTimeEnd;
 
+	@ApiParam(value = "크루 비밀번호.")
 	private String crewPassword;
 	
+	@ApiParam(value = "참가비")
 	private Integer crewCost;
 	
+	@ApiParam(value = "크루 인원 제한", required = true)
 	private Integer crewMaxMember;
 	
 	public static CrewDto of(CrewEntity crew) {
