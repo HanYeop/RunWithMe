7929f45f4b68f93f1d04f46a9a394b60fe7fab9a
한제규 <anfidthtn@naver.com>
2022-07-27 01:33:21 +0900

    [BE] - ADD : 오늘 자신의 기록이 있는지 여부 조회 기능 추가

--- BE/gumid101/src/main/java/com/ssafy/gumid101/crew/activity/CrewActivityRunDslRepository.java
+++ BE/gumid101/src/main/java/com/ssafy/gumid101/crew/activity/CrewActivityRunDslRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.gumid101.crew.activity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.ssafy.gumid101.dto.RecordParamsDto;
@@ -8,5 +9,8 @@ import com.ssafy.gumid101.entity.RunRecordEntity;
 public interface CrewActivityRunDslRepository{
 
 	List<RunRecordEntity> getRunRecords(RecordParamsDto condition);
+
+	List<RunRecordEntity> findAllByUserEntity_userSeqAndCrewEntity_crewSeqAndRunRecordStartTimeBetween(Long userSeq,
+			Long crewSeq, LocalDateTime start, LocalDateTime end);
 
 }
