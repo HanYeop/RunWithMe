9e6712c7b0346cb7ef16fc77285278f5ac34febd
한제규 <anfidthtn@naver.com>
2022-07-27 15:42:19 +0900

    [BE] - FIX : final 빼고 constructor 추가

--- BE/gumid101/src/main/java/com/ssafy/gumid101/res/UserFileDto.java
+++ BE/gumid101/src/main/java/com/ssafy/gumid101/res/UserFileDto.java
@@ -3,11 +3,13 @@ package com.ssafy.gumid101.res;
 import com.ssafy.gumid101.dto.ImageFileDto;
 import com.ssafy.gumid101.dto.UserDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
 
 @Setter
 @Data
+@AllArgsConstructor
 public class UserFileDto {
 
 	private  UserDto user;
