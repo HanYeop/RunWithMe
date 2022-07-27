240c4a814bff19c949fea49228d77bf7c0e0d46b
super7615 <super7615@naver.com>
2022-07-27 23:14:08 +0900

    [AOS] - UPDATE : 크루 게시판 페이징 로직 수정

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/model/dto/CrewBoardDto.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/model/dto/CrewBoardDto.kt
@@ -5,4 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class CrewBoardDto(
     @SerializedName("crewBoardSeq") val crewBoardSeq: Int,
     @SerializedName("crewBoardContent") val crewBoardContent: String,
+    @SerializedName("crewBoardRegTime") val crewBoardRegTime: String,
+    @SerializedName("userNickName") val userNickName: String,
+    @SerializedName("userSeq") val userSeq: Int,
+    @SerializedName("crewName") val crewName: String
 )
