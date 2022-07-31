691fe4c58110a24a6258a5b348238de1de793801
손광진 <start_so@naver.com>
2022-07-31 15:39:20 +0900

    [BE] fix : responsebody to requestpart

--- BE/gumid101/src/main/java/com/ssafy/gumid101/crew/manager/CrewManagerRestController.java
+++ BE/gumid101/src/main/java/com/ssafy/gumid101/crew/manager/CrewManagerRestController.java
@@ -41,104 +41,98 @@ public class CrewManagerRestController {
 	private final JwtUtilsService jwtUtilService;
 	private final CrewManagerService crewManagerService;
 	private final ObjectMapper objectMapper;
-	
+
 	private UserDto loadUserFromToken() {
 		Authentication autentication = SecurityContextHolder.getContext().getAuthentication();
 		UserDto tokenUser = (UserDto) autentication.getPrincipal();
 		return tokenUser;
 	}
-	
-	
 
-	@ApiOperation(value="자신이 해당 크루 소속인지 조회")
+	@ApiOperation(value = "자신이 해당 크루 소속인지 조회")
 	@GetMapping("/{crewSeq}/membercheck")
-	public ResponseEntity<?> crewMemberCheck(@PathVariable Long crewSeq) throws Exception{
-		
-		UserDto userDto= loadUserFromToken();
-		
-		Boolean check =  crewManagerService.isUserCrewMember(userDto.getUserSeq(), crewSeq);
-		
+	public ResponseEntity<?> crewMemberCheck(@PathVariable Long crewSeq) throws Exception {
+
+		UserDto userDto = loadUserFromToken();
+
+		Boolean check = crewManagerService.isUserCrewMember(userDto.getUserSeq(), crewSeq);
+
 		ResponseFrame<?> res = ResponseFrame.of(check, 0, "자신이 해당 크루원인지 여부가 조회되었습니다.");
-		
-		
-		return new ResponseEntity<>(res,HttpStatus.OK);
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * 
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	
-	
-	@ApiOperation(value="진행 중인 내 크루보기 정렬 아직 없음")
+
+	@ApiOperation(value = "진행 중인 내 크루보기 정렬 아직 없음")
 	@GetMapping("/my-current-crew")
-	public ResponseEntity<?> getMyCurrentCrew() throws Exception{
-		
-		UserDto userDto= loadUserFromToken();
-		
-		List<?> crewList =  crewManagerService.getMyCurrentCrew(userDto.getUserSeq());
-		
+	public ResponseEntity<?> getMyCurrentCrew() throws Exception {
+
+		UserDto userDto = loadUserFromToken();
+
+		List<?> crewList = crewManagerService.getMyCurrentCrew(userDto.getUserSeq());
+
 		ResponseFrame<?> res = ResponseFrame.of(crewList, crewList.size(), "현재 진행중, 진행 예정인 나의 현재 크루가 반환되었습니다.");
-		
-		
-		return new ResponseEntity<>(res,HttpStatus.OK);
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
-	
-	
+
 	@ApiOperation("내 끝난 크루를 조회 why? 업적 조회할 떄,")
 	@GetMapping("/my-end-crew")
-	public RequestEntity<?> getMyEndCrew(){
+	public RequestEntity<?> getMyEndCrew() {
 		return null;
 	}
-	
+
 	/**
 	 * 
 	 * @param paramsDto
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@ApiOperation("모집 중인 크루 리스트 보기")
 	@GetMapping("/recruitment")
-	public ResponseEntity<?> getCrewRecruitment(@ModelAttribute RecruitmentParamsDto paramsDto) throws Exception{
-		
+	public ResponseEntity<?> getCrewRecruitment(@ModelAttribute RecruitmentParamsDto paramsDto) throws Exception {
 
-		
-		List<CrewFileDto> crewList =  crewManagerService.crewSearcheByRecruitmentParams(paramsDto);
-		if(crewList == null) {
+		List<CrewFileDto> crewList = crewManagerService.crewSearcheByRecruitmentParams(paramsDto);
+		if (crewList == null) {
 			crewList = new ArrayList<CrewFileDto>();
 		}
 		ResponseFrame<?> res = ResponseFrame.of(crewList, crewList.size(), "모집중인 크루 리스트를 반환합니다.");
-		
-		return new ResponseEntity<>(res,HttpStatus.OK);
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * 크루 생성하기
+	 * 
 	 * @param crewDto
 	 * @return
 	 */
 	@ApiOperation(value = "크루 생성하기(모집하기)")
 	@PostMapping("/crew")
-	public ResponseEntity<?> createCrew(@RequestPart("crewDto") String crewDto, @RequestPart(name = "img", required = false) MultipartFile image) throws Exception{
+	public ResponseEntity<?> createCrew(@RequestPart("crewDto") String crewDto,
+			@RequestPart(name = "imgFile", required = false) MultipartFile image) throws Exception {
 
 		UserDto managerDto = loadUserFromToken();
 		HttpStatus httpStatus = HttpStatus.OK;
 		ResponseFrame<CrewFileDto> responseMap = new ResponseFrame<>();
-		
+
 		CrewDto crewteCrewDto = objectMapper.readValue(crewDto, CrewDto.class);
 		crewteCrewDto.setCrewMemberCount(1);
 		CrewFileDto crewFileDto = null;
 		try {
 			crewFileDto = crewManagerService.createCrew(image, crewteCrewDto, managerDto);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			httpStatus = HttpStatus.OK;
 			responseMap.setCount(0);
 			responseMap.setData(new CrewFileDto(null, null));
 			responseMap.setSuccess(false);
 			responseMap.setMsg(e.getMessage());
 		}
-		
+
 		if (crewFileDto != null) {
 			responseMap.setCount(1);
 			responseMap.setSuccess(true);
@@ -147,37 +141,37 @@ public class CrewManagerRestController {
 		responseMap.setData(crewFileDto);
 		return new ResponseEntity<>(responseMap, httpStatus);
 	}
-	
+
 	/**
 	 * 크루 삭제하기 , 크루장만 가능 , 조건 - 시작일 전만
+	 * 
 	 * @param crewSeq
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@ApiOperation(value = "크루장이 크루 해체")
 	@DeleteMapping("/crew/{crewSeq}")
-	public ResponseEntity<?> deleteCrew(@PathVariable(required = true) long crewSeq) throws Exception{
-		
+	public ResponseEntity<?> deleteCrew(@PathVariable(required = true) long crewSeq) throws Exception {
+
 		UserDto userDto = loadUserFromToken();
-		
-		int result = crewManagerService.deleteCrew(crewSeq,userDto.getUserSeq());
-		
-		ResponseFrame<Integer> res = ResponseFrame.of(result,result,"크루장의 크루삭제가 완료되었습니다.");
-		
+
+		int result = crewManagerService.deleteCrew(crewSeq, userDto.getUserSeq());
+
+		ResponseFrame<Integer> res = ResponseFrame.of(result, result, "크루장의 크루삭제가 완료되었습니다.");
+
 		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/crew/{crewSeq}")
-	public ResponseEntity<?> getCrewDetail(@PathVariable Long crewSeq) throws Exception{
-		
-		CrewFileDto  crewFileDto = crewManagerService.getCrewDetail(crewSeq);
-		
+	public ResponseEntity<?> getCrewDetail(@PathVariable Long crewSeq) throws Exception {
+
+		CrewFileDto crewFileDto = crewManagerService.getCrewDetail(crewSeq);
+
 		ResponseFrame<CrewFileDto> resFrame = ResponseFrame.of(crewFileDto, 1, "크루 상세정보를 반환합니다.");
-				
-		
-		return new ResponseEntity<>(resFrame,HttpStatus.OK);
+
+		return new ResponseEntity<>(resFrame, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * 
 	 * @param crewSeq
@@ -185,33 +179,33 @@ public class CrewManagerRestController {
 	 * @return
 	 */
 //	@PostMapping("/crew/{crewSeq}/user")
-	//public RequestEntity<?> joinCrew(@PathVariable long crewSeq, @RequestParam String password){
-	//	return null;//이거 crewController에 있는 듯?
-	//}
-	
+	// public RequestEntity<?> joinCrew(@PathVariable long crewSeq, @RequestParam
+	// String password){
+	// return null;//이거 crewController에 있는 듯?
+	// }
+
 	/**
 	 * 크루원이 크루 시작전 크루 탈퇴
+	 * 
 	 * @param crewSeq
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@ApiOperation("크루 탈퇴")
 	@DeleteMapping("/crew/{crewSeq}/user")
-	public ResponseEntity<?> exitCrew(@PathVariable long crewSeq) throws Exception{
-		UserDto user =  loadUserFromToken();
-		
-		int result = crewManagerService.exitCrew(crewSeq,user.getUserSeq());
-		
+	public ResponseEntity<?> exitCrew(@PathVariable long crewSeq) throws Exception {
+		UserDto user = loadUserFromToken();
+
+		int result = crewManagerService.exitCrew(crewSeq, user.getUserSeq());
+
 		ResponseFrame<?> frame;
-		if(1==result) {
+		if (1 == result) {
 			frame = ResponseFrame.of(result, result, "정상적으로 탈퇴되었습니다.");
-		}else {
+		} else {
 			frame = ResponseFrame.of(false, "탈퇴 중에 오류 발생");
 		}
-		
-		return new ResponseEntity<>(frame,HttpStatus.OK);
+
+		return new ResponseEntity<>(frame, HttpStatus.OK);
 	}
-	
-	
 
 }
