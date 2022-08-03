aac6dc1b3f66c7a220a1758c9eec49b86e033c3e
super7615 <super7615@naver.com>
2022-08-03 15:30:40 +0900

    [AOS] - ADD : 자동 로그인

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/LoginActivity.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/login/LoginActivity.kt
@@ -1,14 +1,31 @@
 package com.ssafy.runwithme.view.login
 
+import android.content.Intent
+import android.content.SharedPreferences
+import com.ssafy.runwithme.MainActivity
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseActivity
 import com.ssafy.runwithme.databinding.ActivityMainBinding
+import com.ssafy.runwithme.utils.JWT
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_login) {
 
-    override fun init() {
+    @Inject
+    lateinit var sharedPref: SharedPreferences
 
+    override fun init() {
+        autoLogin()
+    }
+
+    private fun autoLogin(){
+        val jwt = sharedPref.getString(JWT, "")
+
+        if(jwt != ""){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
