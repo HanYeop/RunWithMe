bea5d5b09a5dd910847fd6079f4607bfe4081c6c
super7615 <super7615@naver.com>
2022-08-04 23:46:45 +0900

    [AOS] - TEST : 네이버 로그아웃

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/others/OthersFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/my_page/others/OthersFragment.kt
@@ -3,8 +3,13 @@ package com.ssafy.runwithme.view.my_page.others
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
+import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
+import com.navercorp.nid.oauth.OAuthLoginCallback
 import com.ssafy.runwithme.BuildConfig
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
@@ -54,7 +59,29 @@ class OthersFragment : BaseFragment<FragmentOthersBinding>(R.layout.fragment_oth
     }
 
     private fun logout(){
+        startNaverDeleteToken()
+
         myPageViewModel.deleteFcmToken()
+    }
+
+    private fun startNaverDeleteToken(){
+        NidOAuthLogin().callDeleteTokenApi(requireContext(), object : OAuthLoginCallback {
+            override fun onSuccess() {
+                //서버에서 토큰 삭제에 성공한 상태입니다.
+                Toast.makeText(requireContext(), "네이버 아이디 토큰삭제 성공!", Toast.LENGTH_SHORT).show()
+            }
+            override fun onFailure(httpStatus: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                Log.d("naver", "errorCode: ${NaverIdLoginSDK.getLastErrorCode().code}")
+                Log.d("naver", "errorDesc: ${NaverIdLoginSDK.getLastErrorDescription()}")
+            }
+            override fun onError(errorCode: Int, message: String) {
+                // 서버에서 토큰 삭제에 실패했어도 클라이언트에 있는 토큰은 삭제되어 로그아웃된 상태입니다.
+                // 클라이언트에 토큰 정보가 없기 때문에 추가로 처리할 수 있는 작업은 없습니다.
+                onFailure(errorCode, message)
+            }
+        })
     }
 
     private fun initViewModelCallBack(){
