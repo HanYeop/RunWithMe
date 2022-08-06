4cd69eb3e83040ef7ab99619b8ac9571e3cb0b30
super7615 <super7615@naver.com>
2022-08-06 23:23:09 +0900

    [AOS] - TEST : 러닝 Result

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/running/result/RunningResultActivity.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/running/result/RunningResultActivity.kt
@@ -54,28 +54,28 @@ class RunningResultActivity : BaseActivity<ActivityRunningResultBinding>(R.layou
     @Inject
     lateinit var sharedPreferences: SharedPreferences
 
-    private var POLYLINE_COLOR = Color.RED
-
-    // 구글맵 선언
-    private var map: GoogleMap? = null
+//    private var POLYLINE_COLOR = Color.RED
+//
+//    // 구글맵 선언
+//    private var map: GoogleMap? = null
 
     private lateinit var loadingDialog : LoadingDialog
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        POLYLINE_COLOR = resources.getColor(R.color.main_purple)
-
-        binding.apply {
-            mapView.onCreate(savedInstanceState)
-            // 맵 불러오기
-            mapView.getMapAsync {
-                map = it
-                addAllPolylines()
-                zoomToWholeTrack()
-            }
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        POLYLINE_COLOR = resources.getColor(R.color.main_purple)
+//
+//        binding.apply {
+//            mapView.onCreate(savedInstanceState)
+//            // 맵 불러오기
+//            mapView.getMapAsync {
+//                map = it
+//                addAllPolylines()
+//                zoomToWholeTrack()
+//            }
+//        }
+//    }
     
     override fun init() {
         initClickListener()
@@ -128,45 +128,46 @@ class RunningResultActivity : BaseActivity<ActivityRunningResultBinding>(R.layou
         runningViewModel.createCoordinates(runRecordSeq, list)
     }
 
-    // 경로 전부 표시
-    private fun addAllPolylines() {
-        lifecycleScope.launch {
-            for (polyline in RunningActivity.runPathPoints) {
-                for(i in 1 until polyline.size){
-                    val polylineOptions = PolylineOptions()
-                        .color(POLYLINE_COLOR)
-                        .width(POLYLINE_WIDTH)
-                        .add(polyline[i])
-                        .add(polyline[i - 1])
-                    map?.addPolyline(polylineOptions)
-                    delay(100)
-                }
-            }
-        }
-    }
-
-    // 스냅샷 찍기 위하여 전체 경로가 다 보이게 줌
-    private fun zoomToWholeTrack() {
-        val bounds = LatLngBounds.Builder()
-        for (polyline in RunningActivity.runPathPoints) {
-            for (point in polyline) {
-                bounds.include(point)
-            }
-        }
-        val width = binding.mapView.width
-        val height = binding.mapView.height
-        map?.moveCamera(
-            CameraUpdateFactory.newLatLngBounds(
-                bounds.build(),
-                width,
-                height,
-                (height * 0.05f).toInt()
-            )
-        )
-    }
+//    // 경로 전부 표시
+//    private fun addAllPolylines() {
+//        lifecycleScope.launch {
+//            for (polyline in RunningActivity.runPathPoints) {
+//                for(i in 1 until polyline.size){
+//                    val polylineOptions = PolylineOptions()
+//                        .color(POLYLINE_COLOR)
+//                        .width(POLYLINE_WIDTH)
+//                        .add(polyline[i])
+//                        .add(polyline[i - 1])
+//                    map?.addPolyline(polylineOptions)
+//                    delay(100)
+//                }
+//            }
+//        }
+//    }
+//
+//    // 스냅샷 찍기 위하여 전체 경로가 다 보이게 줌
+//    private fun zoomToWholeTrack() {
+//        val bounds = LatLngBounds.Builder()
+//        for (polyline in RunningActivity.runPathPoints) {
+//            for (point in polyline) {
+//                bounds.include(point)
+//            }
+//        }
+//        val width = binding.mapView.width
+//        val height = binding.mapView.height
+//        map?.moveCamera(
+//            CameraUpdateFactory.newLatLngBounds(
+//                bounds.build(),
+//                width,
+//                height,
+//                (height * 0.05f).toInt()
+//            )
+//        )
+//    }
 
     private fun initResult(){
         binding.apply {
+            imageView.setImageBitmap(RunningActivity.image)
             tvSpeed.text = "${RunningActivity.runRecordRunningAvgSpeed} km/h"
             tvCalorie.text = "${RunningActivity.runRecordRunningCalorie} kcal"
             tvTime.text = "${getFormattedStopWatchTime(RunningActivity.runRecordRunningTime,false)}"
@@ -263,31 +264,31 @@ class RunningResultActivity : BaseActivity<ActivityRunningResultBinding>(R.layou
         }
     }
 
-    /**
-     * 라이프 사이클에 맞게 맵뷰를 처리해줌
-     */
-    override fun onResume() {
-        binding.mapView.onResume()
-        super.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        binding.mapView.onStart()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        binding.mapView.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        binding.mapView.onStop()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        binding.mapView.onLowMemory()
-    }
+//    /**
+//     * 라이프 사이클에 맞게 맵뷰를 처리해줌
+//     */
+//    override fun onResume() {
+//        binding.mapView.onResume()
+//        super.onResume()
+//    }
+//
+//    override fun onStart() {
+//        super.onStart()
+//        binding.mapView.onStart()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        binding.mapView.onPause()
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        binding.mapView.onStop()
+//    }
+//
+//    override fun onLowMemory() {
+//        super.onLowMemory()
+//        binding.mapView.onLowMemory()
+//    }
 }
