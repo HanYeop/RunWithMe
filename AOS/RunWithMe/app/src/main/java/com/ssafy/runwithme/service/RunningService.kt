cadfa700c62c955840dacd830de1e812bde7a9bd
super7615 <super7615@naver.com>
2022-08-11 20:33:58 +0900

    [AOS] - UPDATE : 비정상 이동 감지 (5~7초 100m)

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/service/RunningService.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/service/RunningService.kt
@@ -283,8 +283,8 @@ class RunningService : LifecycleService() {
                 pauseService()
             }
 
-            // 5초 이상 이동했는데 이동거리가 150m 이상인 경우 정지
-            if(result[0] > 150f && (System.currentTimeMillis() - startTime) > 5000L) {
+            // 5초 이상 이동했는데 이동거리가 100m 이상인 경우 정지
+            if(result[0] > 100f && (System.currentTimeMillis() - startTime) > 5000L) {
                 Toast.makeText(this, "비정상적인 이동이 감지되어 러닝이 일시 중지되었습니다.", Toast.LENGTH_SHORT).show()
                 ttsSpeak("비정상적인 이동이 감지되어 러닝이 일시 중지되었습니다.")
                 pauseService()
