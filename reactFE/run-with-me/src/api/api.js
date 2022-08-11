d5af0a4a467dd9eca46d768a57164ce4bc7c7ae8
손광진 <start_so@naver.com>
2022-08-11 21:52:11 +0900

    [BE] FIX : 리액트 배포환경에 맞게 수정

--- reactFE/run-with-me/src/api/api.js
+++ reactFE/run-with-me/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://i7d101.p.ssafy.io/api",
 });
 
 export default axiosClient;
