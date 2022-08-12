37e3d62b1d0edd1be42653fdd55419354aefebf4
dkeka1187 <dkeka1187@naver.com>
2022-08-12 17:17:06 +0900

    [AOS] - ADD : 시즌 랭킹 페이지 추가

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/competition/CompetitionRankingFragment.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/view/competition/CompetitionRankingFragment.kt
@@ -3,21 +3,19 @@ package com.ssafy.runwithme.view.competition
 import androidx.navigation.fragment.findNavController
 import com.ssafy.runwithme.R
 import com.ssafy.runwithme.base.BaseFragment
-import com.ssafy.runwithme.databinding.FragmentCompetitionBinding
+import com.ssafy.runwithme.databinding.FragmentCompetitionRankingBinding
 
-class CompetitionFragment : BaseFragment<FragmentCompetitionBinding>(R.layout.fragment_competition) {
+class CompetitionRankingFragment : BaseFragment<FragmentCompetitionRankingBinding>(R.layout.fragment_competition_ranking) {
 
     override fun init() {
         initClickListener()
     }
 
+
     private fun initClickListener(){
         binding.apply {
             toolbar.setNavigationOnClickListener {
                 findNavController().popBackStack()
-            }
-            cardRanking.setOnClickListener {
-                findNavController().navigate(R.id.action_competitionFragment_to_competitionRankingFragment)
             }
         }
     }
