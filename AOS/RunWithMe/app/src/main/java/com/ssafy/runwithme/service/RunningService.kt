d7f39c8a555c584746b87ee7e96b52ed5ed3770f
super7615 <super7615@naver.com>
2022-08-15 14:07:53 +0900

    [AOS] - UPDATE : 정지 조건 3m -> 2.3m

--- AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/service/RunningService.kt
+++ AOS/RunWithMe/app/src/main/java/com/ssafy/runwithme/service/RunningService.kt
@@ -274,8 +274,8 @@ class RunningService : LifecycleService() {
 
             Log.d(TAG, "distancePolyline: ${result[0]}")
 
-            // 5초 이상 이동했는데 이동거리가 3m 이하인 경우 정지하고, 마지막 위치를 기록함 (최소 오차 4초)
-            if(result[0] < 3f && (System.currentTimeMillis() - startTime) > 4000L) {
+            // 5초 이상 이동했는데 이동거리가 2.3m 이하인 경우 정지하고, 마지막 위치를 기록함 (최소 오차 4초)
+            if(result[0] < 2.3f && (System.currentTimeMillis() - startTime) > 4000L) {
 
                 Toast.makeText(this, "이동이 없어 러닝이 일시 중지되었습니다.", Toast.LENGTH_SHORT).show()
                 ttsSpeak("이동이 없어 러닝이 일시 중지되었습니다.")
